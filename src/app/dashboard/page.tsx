3aba5926a780b84d621cdc6ded663192c42d06de
jayasrir12 <JayasriR@chimeratechnologies.com>
2025-09-29 14:43:30 +0530

    status radial chart added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,7 +29,17 @@ import type { ProjectType } from "@/types/project.type";
 import type { IssueType } from "@/types/issue.type";
 
 import { Header } from "../_components/header";
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  LabelList,
+  RadialBar,
+  RadialBarChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 export default function DashboardPage() {
   const {
@@ -49,7 +59,7 @@ export default function DashboardPage() {
   //project data
   const projectsData = projectsResponse.projects;
   const [selectedProject, setSelectedProject] = useState<ProjectType | null>(
-    null,
+    null
   );
 
   // issues of selected project
@@ -62,7 +72,7 @@ export default function DashboardPage() {
     const fetchIssues = async () => {
       try {
         const res = await axios.get<{ issues: IssueType[] }>(
-          `etl/projects/${selectedProject.id}/issues`,
+          `etl/projects/${selectedProject.id}/issues`
         );
         setIssues(res.data.issues);
       } catch (err) {
@@ -73,6 +83,20 @@ export default function DashboardPage() {
     fetchIssues();
   }, [selectedProject]);
 
+  // Count issues by status
+  const statusData = [
+    {
+      status: "Done",
+      count: issues.filter((i) => i.status === "Done").length,
+      fill: "#0a963dff",
+    },
+    {
+      status: "Other",
+      count: issues.filter((i) => i.status !== "Done").length,
+      fill: "#9c6403ff",
+    },
+  ];
+
   if (projectLoading) return <p>Loading...</p>;
   if (projectError) return <p>Error in fetching Project data</p>;
   if (!projectsData) return <p>No project Data</p>;
@@ -82,7 +106,7 @@ export default function DashboardPage() {
       <Header />
 
       {/* select project */}
-      <div className="flex gap-4 mt-6 mb-6">
+      <div className='flex gap-4 mt-6 mb-6'>
         <Select
           value={selectedProject?.key}
           onValueChange={(value) => {
@@ -91,12 +115,12 @@ export default function DashboardPage() {
             if (proj) {
               //project data into selected project
               setSelectedProject(proj);
-              setSelectedIssueId(null); 
+              setSelectedIssueId(null);
             }
           }}
         >
-          <SelectTrigger className="w-[220px]">
-            <SelectValue placeholder="Select a Project" />
+          <SelectTrigger className='w-[220px]'>
+            <SelectValue placeholder='Select a Project' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -116,8 +140,8 @@ export default function DashboardPage() {
           onValueChange={(value) => setSelectedIssueId(value)}
           disabled={!selectedProject || issues.length === 0}
         >
-          <SelectTrigger className="w-[220px]">
-            <SelectValue placeholder="Select an Issue" />
+          <SelectTrigger className='w-[220px]'>
+            <SelectValue placeholder='Select an Issue' />
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -132,49 +156,76 @@ export default function DashboardPage() {
         </Select>
       </div>
       <div>
+        <div className='flex gap-6 mt-6'>
+          {/*Radial Chart */}
+          <Card className='flex-1'>
+            <CardHeader>
+              <CardTitle>Status Chart</CardTitle>
+              <CardDescription>{selectedProject?.name}</CardDescription>
+            </CardHeader>
+            <CardContent className='flex justify-center'>
+              <RadialBarChart
+                width={300}
+                height={300}
+                data={statusData}
+                startAngle={90}
+                endAngle={-270}
+                innerRadius={20}
+                outerRadius={100}
+              >
+                <Tooltip
+                  content={({ payload }) => {
+                    if (!payload || payload.length === 0) return null;
+                    const data = payload[0].payload;
+                    return (
+                      <div className='bg-black text-white px-2 py-1 rounded text-sm'>
+                        {data.status}: {data.count}
+                      </div>
+                    );
+                  }}
+                />
 
-        {/* bar chart */}
-        <Card>
-          <CardHeader>
-            <CardTitle>
-              {selectedProject?.name} ({selectedProject?.key})
-            </CardTitle>
-            <CardDescription>January - June 2024</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div>
-              {selectedIssueId
-                ? `Issue - ${selectedIssueId}`
-                : "No issue selected"}
-            </div>
-              <Card className="mb-6">
-        <CardHeader>
-        
-          <CardDescription>{selectedProject?.name}</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <BarChart width={600} height={300}  margin={{ top: 20 }}>
-            <CartesianGrid strokeDasharray="3 3" vertical={false} />
-            <XAxis dataKey="label" />
-            <YAxis />
-            <Tooltip />
-            <Bar dataKey="count" fill="#201d61ff" radius={8} />
-          </BarChart>
-        </CardContent>
-      </Card>
-          </CardContent>
-          <CardFooter className="flex-col items-start gap-2 text-sm">
-            <div className="flex gap-2 leading-none font-medium">
-              Trending up by 5.2% this month{" "}
-              <TrendingUp className="h-4 w-4" />
-            </div>
-            <div className="text-muted-foreground leading-none">
-              Showing total visitors for the last 6 months
-            </div>
-          </CardFooter>
-        </Card>
+                <RadialBar dataKey='count' background>
+                  <LabelList
+                    dataKey='status'
+                    position='insideStart'
+                    className='fill-white font-bold'
+                  />
+                </RadialBar>
+              </RadialBarChart>
+            </CardContent>
+            <CardFooter>Total issues: {issues.length}</CardFooter>
+          </Card>
+
+          {/* Bar Chart */}
+          <Card className='flex-1'>
+            <CardHeader>
+              <CardTitle>
+                {selectedProject?.name} ({selectedProject?.key})
+              </CardTitle>
+              <CardDescription>January - June 2024</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <BarChart width={500} height={300} margin={{ top: 20 }}>
+                <CartesianGrid strokeDasharray='3 3' vertical={false} />
+                <XAxis dataKey='label' />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey='count' fill='#201d61ff' radius={8} />
+              </BarChart>
+            </CardContent>
+            <CardFooter className='flex-col items-start gap-2 text-sm'>
+              <div className='flex gap-2 leading-none font-medium'>
+                Trending up by 5.2% this month{" "}
+                <TrendingUp className='h-4 w-4' />
+              </div>
+              <div className='text-muted-foreground leading-none'>
+                Showing total visitors for the last 6 months
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
     </div>
   );
 }
-
